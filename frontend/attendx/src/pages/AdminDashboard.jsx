390ce2a2c0c7192fd17caea0a35e98ac74205425
chandrasekhar <bcsr557@gmail.com>
2026-05-21 15:33:32 +0530

    commit

--- frontend/attendx/src/pages/AdminDashboard.jsx
+++ frontend/attendx/src/pages/AdminDashboard.jsx
@@ -252,8 +252,7 @@ const AdminDashboard = () => {
                   accept=".xlsx,.xls"
                   onChange={handleImportStaff}
                   style={{ display: 'none' }}
-                  disabled={isUploading}
-                />
+             ></input>
               </>
             )}
             <button className="btn btn-secondary" onClick={exportToCSV} disabled={isExporting}>
